6cbc6d674ca0c316df099455a485a76520a420eb
Datom-code <enochgbadebo007@gmail.com>
2025-09-09 11:52:14 +0100

    future_age_calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+# print(int(input("How old are you? ")))
+# age = input 
+# print("In 2025, you will be", age, "years old.")
+
+age = int(input("How old are you? "))
+print("In 2025, you will be", age + 27, "years old.")
