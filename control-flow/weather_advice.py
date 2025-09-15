f7a4774e7892a6cf97ebaabd52f1eb18ddf7e817
Datom-code <enochgbadebo007@gmail.com>
2025-09-15 16:02:54 +0100

    added weather advice

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+weather = input(" What's the weather like today? (sunny/rainy/cold):.")
+if weather == "sunny":
+    print("It's a beautiful day! Don't forget your sunglasses.")
+elif weather == "rainy":
+    print("It's raining! Take an umbrella with you.")
+elif weather == "cold":
+    print("It's cold outside! Wear a warm coat.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,4 +2,4 @@ principal = 1000
 rate = 0.05
 time = 3
 interest = principal * time * rate
-print("The simple interest is:", interest)
+print("The simple interest is:", interest) 
