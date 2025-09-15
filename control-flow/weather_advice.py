e2ef5e4ae01b4ca0b97ceed431d6c10d87b0326c
Datom-code <enochgbadebo007@gmail.com>
2025-09-15 16:06:43 +0100

    made changes to weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
-weather = input(" What's the weather like today? (sunny/rainy/cold):.")
+weather = input(" What's the weather like today? (sunny/rainy/cold):")
 if weather == "sunny":
-    print("It's a beautiful day! Don't forget your sunglasses.")
+    print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-    print("It's raining! Take an umbrella with you.")
+    print("Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
-    print("It's cold outside! Wear a warm coat.")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
