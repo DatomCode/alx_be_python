2e76df5433dfe7444ea4c6121387b1fab342f9e6
Datom-code <enochgbadebo007@gmail.com>
2025-10-11 18:30:14 +0100

    added polymorphism_demo

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,26 @@
+import math
+
+
+class Shape:
+    def area(self):
+        raise NotImplementedError
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        
+        self.length = length
+        self.width = width
+
+    def area(self):
+        area = self.length * self.width
+        return f"The area of the Rectangle is: {area}"
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        area = math.pi * (self.radius ** 2)
+        return f"The area of the Circle is: {area}"
